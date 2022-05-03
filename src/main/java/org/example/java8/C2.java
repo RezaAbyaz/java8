2e017fc468da0b7c76123f2f02deaeca40f2c62e
Administrator <reza.abyaz@yahoo.com>
2022-05-03 12:38:43 +0430

    add test package to project

--- src/main/java/org/example/java8/C2.java
+++ src/main/java/org/example/java8/C2.java
@@ -4,9 +4,11 @@ import java.util.Comparator;
 
 public class C2  implements Comparator {
 
-    @Override
-    public int compare(Object o1, Object o2) {
 
-        return ((Student) o1).getLastName().compareToIgnoreCase(((Student) o2).getLastName());
+
+    public void tt(){}
+
+    public int compare(Object o1, Object o2) {
+        return 0;
     }
 }
